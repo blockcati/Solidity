09c6e3ee1bb9151a94b7d2ec69d2afd610afef76
blockcati <aniqueali11@gmail.com>
2025-01-14 01:10:28 +0500

    Basic Calculator

---
+++ contracts/Funtions1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Calculator{
+
+function Addnums(uint num1, uint num2) public pure returns (uint){
+    return num1 + num2;
+}
+
+}
